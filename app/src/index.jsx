bdd2721ad8c6f3defcac7ca11ceb7911a5dbad75
Tony Huang <thua9118@uni.sydney.edu.au>
2021-09-28 20:18:53 +1000

    linux is case sensitive sadly

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Creator from './components/creator/Creator'
+import Creator from './components/creator/creator';
 
-import './index.scss'
+import './index.scss';
 
-import "@blueprintjs/core/lib/css/blueprint.css";
+import '@blueprintjs/core/lib/css/blueprint.css';
 import 'normalize.css';
 
-const App = function() {
+const App = function () {
   return (
     <>
       <Creator />
     </>
-  )
-}
+  );
+};
 
-const view = App('PySOM Creator')
+const view = App('PySOM Creator');
 
-const element = document.getElementById('app')
-ReactDOM.render(view, element)
+const element = document.getElementById('app');
+ReactDOM.render(view, element);
