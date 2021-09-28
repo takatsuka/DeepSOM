5dec0a3726a4009f6dbf9af4f69207c6c7dc8418
HaoYan Qi <haoyanqi666@gmail.com>
2021-09-28 12:30:35 +0800

    pep8 format index.py

--- app/src/index.py
+++ app/src/index.py
@@ -18,48 +18,49 @@ class Api:
         with open(filename, 'w') as f:
             f.write(content)
 
-
     def open_csv_file(self):
         filename = webview.windows[0].create_file_dialog(webview.OPEN_DIALOG)
-        if filename == None: return None
+        if filename == None:
+            return None
 
-        if len(filename) < 1: return None
+        if len(filename) < 1:
+            return None
         filename = filename[0]
-        if not os.path.exists(filename): return None
-        
+        if not os.path.exists(filename):
+            return None
+
         lines = open(filename).readlines()
         return [l.strip().split(',') for l in lines]
-
 
     def terminate(self):
         webview.windows[0].destroy()
         os.exit(0)
 
 
-
-
 def get_index_page():
     def exists(path):
-        if hasattr(sys,'_MEIPASS'):
+        if hasattr(sys, '_MEIPASS'):
             print(f"searching {os.path.join(os.getcwd(), path)}")
             return os.path.exists(os.path.join(os.getcwd(), path))
         else:
             return os.path.exists(os.path.join(os.path.dirname(__file__), path))
 
-    if exists('../gui/index.html'): # unfrozen development
+    if exists('../gui/index.html'):  # unfrozen development
         return '../gui/index.html'
 
     if exists('./gui/index.html'):
         return './gui/index.html'
-    
+
     if exists('./gui/index.html'):
         return './gui/index.html'
 
     raise Exception(f'No index.html found {os.getcwd()} {sys._MEIPASS}')
 
 
-if hasattr(sys,'_MEIPASS'):
+if hasattr(sys, '_MEIPASS'):
     print("moving to forzen application path: " + sys._MEIPASS)
     os.chdir(sys._MEIPASS)
-window = webview.create_window('PySOM Creator', get_index_page(), js_api=Api(), frameless=True, easy_drag=False)
-webview.start(debug=(False if hasattr(sys,'_MEIPASS') else True))  # gui="cef")
+window = webview.create_window('PySOM Creator', get_index_page(
+), js_api=Api(), frameless=True, easy_drag=False)
+# gui="cef")
+webview.start(debug=(False if hasattr(sys, '_MEIPASS') else True))
