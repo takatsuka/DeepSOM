0471cb578084c3ab6d3a8f164032cf914b1bc950
Tony Huang <thua9118@uni.sydney.edu.au>
2021-09-29 17:52:05 +1000

    linting

--- tests/pysom_tests/utils/test_divide.py
+++ tests/pysom_tests/utils/test_divide.py
@@ -5,7 +5,7 @@ from pysom.utils.divide import divide
 class DivideTester(unittest.TestCase):
     def test_divide_basic(self):
         self.assertAlmostEqual(7.0, divide(14.0, 2.0))
-        
+
     def test_divide_good_nonint(self):
         self.assertAlmostEqual(1.5, divide(3.0, 2.0))
 
