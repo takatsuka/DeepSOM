4a8ae5e431437670db450110dc03b78b952ac0a7
Tony Huang <thua9118@uni.sydney.edu.au>
2021-09-29 11:19:30 +1000

    add random calculator package structure

---
+++ pysom/utils/add.py
@@ -0,0 +1,12 @@
+def add(arg1: float, arg2: float) -> float:
+    """
+    Performs an add Calculator operation on two floats and returns a float.
+
+    Args:
+        arg1 (float): the first float operand
+        arg2 (float): the second float operand
+
+    Returns:
+        float: the addition of arg1 and arg2
+    """
+    return float(arg1 + arg2)
---
+++ pysom/utils/subtract.py
@@ -0,0 +1,13 @@
+def subtract(arg1: float, arg2: float) -> float:
+    """
+    Performs a subtraction Calculator operation on two floats and
+    returns a float.
+
+    Args:
+        arg1 (float): the first float operand
+        arg2 (float): the second float operand
+
+    Returns:
+        float: the subtraction of arg1 by arg2
+    """
+    return float(arg1 - arg2)
