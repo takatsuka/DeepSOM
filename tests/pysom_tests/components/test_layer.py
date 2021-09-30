bb9e9437addb1d95f14ee7622117ee269e7a7584
Michael <mpod2632@uni.sydney.edu.au>
2021-10-01 00:47:34 +1000

    som container w layer tests

--- tests/pysom_tests/components/test_layer.py
+++ tests/pysom_tests/components/test_layer.py
@@ -1,5 +1,8 @@
 from pysom.components.som import Som
 from pysom.components.layer import Layer
+from pysom.components.som_container import SomContainer
+from pysom.utils.transition_funcs import concat_binary
+import pytest
 
 
 def test_layer_init_inlen():
@@ -26,3 +29,24 @@ def test_set_get_inlen_na():
     layer.set_in_len(7)
     assert hasattr(layer, "inlen")
     assert layer.get_in_len() == 7
+
+
+def test_get_outlen_init():
+    layer = Layer(1)
+    assert layer.get_out_len() == 0
+    layer = Layer(0)
+    assert layer.get_out_len() == 0
+
+
+def test_add_container():
+    layer = Layer(1)
+    assert layer.get_out_len() == 0
+
+    som = Som(3, 3, 2)
+    sc = SomContainer(som, concat_binary)
+
+    layer.add_som_container(sc)
+
+    assert layer.get_layer_size() == 1
+    assert len(layer.soms) == 1
+    assert layer.get_out_len() == sc.get_out_len()
