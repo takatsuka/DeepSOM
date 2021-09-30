f2205f1ca95809bf24893a0e0ba799aa468f8b04
Michael <mpod2632@uni.sydney.edu.au>
2021-09-30 17:05:25 +1000

    fixing lint issues and numpy in pipeline

--- tests/pysom_tests/utils/test_decay_funcs.py
+++ tests/pysom_tests/utils/test_decay_funcs.py
@@ -3,8 +3,8 @@ from pysom.utils.decay_funcs import exp_decay, linear_step
 
 
 def test_exp_decay_basic():
-    assert pytest.approx(0.6958125748377546) == exp_decay(1, 0.7, 0.1, (0.7-0.1)/100)
+    assert pytest.approx(0.6958125748377546) == exp_decay(1, 0.7, 0.1, (0.7 - 0.1) / 100)
 
 
 def test_linear_step_basic():
-    assert pytest.approx(0.694) == linear_step(1, 0.7, 0.1, (0.7-0.1)/100)
+    assert pytest.approx(0.694) == linear_step(1, 0.7, 0.1, (0.7 - 0.1) / 100)
