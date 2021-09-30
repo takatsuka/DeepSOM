84233c82d02670065cabc1de14f6afb4710f8e31
Michael <mpod2632@uni.sydney.edu.au>
2021-09-30 23:54:00 +1000

    test concat binary

---
+++ tests/pysom_tests/utils/test_transition_funcs.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pysom.utils.transition_funcs import concat_binary
+from pysom.components.som import Som
+
+
+def test_concat_binary_basic():
+    vector = np.array([1, 2])
+    som = Som(3, 3, 2)
+
+    expected = np.zeros(som.width * som.height)
+    bmu_coords = som.get_idx_closest(vector)
+    expected[bmu_coords[0] * som.height + bmu_coords[1]] = 1
+
+    output = concat_binary(vector, som)
+
+    assert all([a == b for a, b in zip(expected, output)])
+    assert len(expected) == len(output)
