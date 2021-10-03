582d1d6c6e1233e27302d1939d5a341dd7d2fd85
HaoYan Qi <haoyanqi666@gmail.com>
2021-10-03 09:30:00 +0800

    add new line at the end of file

--- app/src/py/som_viz_service.py
+++ app/src/py/som_viz_service.py
@@ -3,10 +3,12 @@ import webview
 import json
 import numpy as np
 
+
 class SOMVisualizationService:
     def __init__(self):
 
-        self.filename = webview.windows[0].create_file_dialog(webview.OPEN_DIALOG)
+        self.filename = webview.windows[0].create_file_dialog(
+            webview.OPEN_DIALOG)
         self.data = json.loads(open(self.filename[0]).read())
         self.weights = np.array(self.data['weights'])
 
