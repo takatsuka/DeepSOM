158f72b635bcc15106eceb6baaf7979c1b4947df
Tony Huang <thua9118@uni.sydney.edu.au>
2021-10-08 12:03:11 +1100

    interim package solution until tests can be refactored

--- setup.py
+++ setup.py
@@ -9,8 +9,10 @@ setup(
     description='Powerful, extensible Self-Organizing Map library for Python',
     author='COMP3988_T17_01_Group2',
     url='https://bitbucket.org/ChristopherIrving/deep-som-dome/',
-    packages=find_packages(include=['pysom']),
-    package_dir={'pysom': 'pysom'},
+    packages=find_packages(),
+    package_dir={'pysom': 'pysom',
+                 'pysom.components': 'pysom/components',
+                 'pysom.utils': 'pysom/utils'},
     tests_require=['pytest'],
     install_requires=['numpy']
 )
