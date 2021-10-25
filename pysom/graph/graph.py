cd63d98792fbf490942c277fd0b84ce645eac95f
HaoYan Qi <haoyanqi666@gmail.com>
2021-10-25 19:20:10 +0800

    rename functions

--- pysom/graph/graph.py
+++ pysom/graph/graph.py
@@ -3,63 +3,54 @@ from nodetypes.som import SOM
 
 
 class Graph:
-
     uid = 2
 
     def __init__(self):
-        self.start = None
-        self.end = None
-        self.nodes = dict()
-        
-    """
-    HELPER METHODS
-    """
-    def create_start(self) -> Node:
-        node = Node(0, data=0)
-        self.start = node
-        return self.add_node(node)
-    
-    def create_end(self) -> Node:
-        node = Node(1, data=0)
-        self.end = node
-        return self.add_node(node)
-            
-    def create_node(self, node_type=None, data=None) -> Node:
+        self.start = 0
+        self.end = 1
+
+        self.nodes = {
+            0: Node(self.start, data=0),
+            1: Node(self.end, data=0)
+        }
+
+
+    def _create_node(self, node_type=None, data=None) -> Node:
         if node_type is None:
             node = Node(Graph.uid, data=data)
         else:
             node = node_type(Graph.uid, data=data)
-            
+
         Graph.uid += 1
-        
+
         return node
 
-    def create_and_add_node(self, node_type=None, data=None) -> int:
-        node = self.create_node(node_type=node_type, data=data)
-        return self.add_node(node)
-        
+    def create(self, node_type=None, data=None) -> int:
+        node = self._create_node(node_type=node_type, data=data)
+        return self._add_node(node)
+
     def get_nodes(self) -> list:
         return list(self.nodes.values())
-        
-    def add_node(self, node: Node) -> int:
+
+    def _add_node(self, node: Node) -> int:
         self.nodes[node.get_id()] = node
         return node.get_id()
-    
+
     def find_node(self, uid: int) -> Node:
         for node in self.nodes.values():
             if node.get_id() == uid:
                 return node
         return None
 
-    def add_connection(self, node1: int, node2: int, slot: int) -> bool:
+    def connect(self, node1: int, node2: int, slot: int) -> bool:
         input_node = self.find_node(node1)
         output_node = self.find_node(node2)
-        
+
         if (input_node is not None) and (output_node is not None):
             return output_node.add_incoming_connection(input_node, slot)
         else:
             return False
-        
+
     """
     SORTING METHODS
     """
@@ -81,34 +72,31 @@ def example_graph():
 
 if __name__ == "__main__":
     g = Graph()
-    
-    start = g.create_start()
-    end = g.create_end()
-    
+
     # Level 1
-    n2 = g.create_and_add_node(node_type=Node, data=1)
-    n3 = g.create_and_add_node(node_type=Node, data=1)
-    g.add_connection(start, n2, 1)
-    g.add_connection(start, n3, 1)
-    
+    n2 = g.create(node_type=Node, data=1)
+    n3 = g.create(node_type=Node, data=1)
+    g.connect(g.start, n2, 1)
+    g.connect(g.start, n3, 1)
+
     # Bubble
-    n4 = g.create_and_add_node(node_type=Node, data=1)
-    n5 = g.create_and_add_node(node_type=Node, data=1)
-    g.add_connection(n2, n4, 1)
-    g.add_connection(n2, n5, 1)
+    n4 = g.create(node_type=Node, data=1)
+    n5 = g.create(node_type=Node, data=1)
+    g.connect(n2, n4, 1)
+    g.connect(n2, n5, 1)
 
     # Top row
-    n6 = g.create_and_add_node(node_type=Node, data=1)
-    g.add_connection(n4, n6, 1)
-    g.add_connection(n5, n6, 1)
-    
+    n6 = g.create(node_type=Node, data=1)
+    g.connect(n4, n6, 1)
+    g.connect(n5, n6, 1)
+
     # Bottom row
-    n7 = g.create_and_add_node(node_type=Node, data=1)
-    g.add_connection(n3, n7, 1)
-    
+    n7 = g.create(node_type=Node, data=1)
+    g.connect(n3, n7, 1)
+
     # Finishing Connections
-    g.add_connection(n6, end, 1)
-    g.add_connection(n7, end, 1)
+    g.connect(n6, g.end, 1)
+    g.connect(n7, g.end, 1)
 
     # Printing Example Graph
     print(example_graph.__doc__)
@@ -127,4 +115,4 @@ if __name__ == "__main__":
 
     print("OVERALL (result of END)")
     print("========================")
-    print("Result of END:", g.find_node(end).evaluate(), end="\n\n")
+    print("Result of END:", g.find_node(g.end).evaluate(), end="\n\n")
