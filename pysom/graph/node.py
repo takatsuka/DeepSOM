21cf3cc003057274e84f76c9a1f44cbfe1bee35b
Tony Huang <thua9118@uni.sydney.edu.au>
2021-10-25 23:07:48 +1100

    add initial docstring for node.py

--- pysom/graph/node.py
+++ pysom/graph/node.py
@@ -1,33 +1,39 @@
 from __future__ import annotations
-
-"""
-
-Types:
-    0 - SOM Node
-    1 - Get BMU
-    2 - etc. etc.
-"""
+# import numpy as np
 
 
 class Node:
 
     def __init__(self, uid, props=None):
+        """
+        The default Node class used to build a SOM Node.
+
+        The smallest definable element for the Deep SOM Model. Can be connected
+        to other SOM Nodes, and at any time may optionally hold some data for
+        evaluation during the training process.
+
+        Args:
+            uid (int): A unique positive integer ID for the Node
+            props ([type], optional): [description]. Defaults to None.
+        """
         self.uid = uid
         self.type = 0
-        self.data = data  # used for storing actual SOM class
+        self.props = props  # used for storing actual SOM class
         self.incoming = list()  # 2-tuple (output_node, slot)
 
         # self.slots = list()
         # {
         #     0: DefaultConnection(self), # only in SOMNode, unused in DistNode
-        #     1: DistConnection(self, indices), # DistConnection stores the actual indices
+        #     1: DistConnection(self, indices), # DistConnection stores the
+        #        actual indices
         #     2: DistConnection(self, indices), # configured by user
         #     3: DistConnection(self, indices) # configured by user
         # }
+
     def __str__(self) -> str:
         str_rep = "Empty Node {}".format(self.uid)
         return str_rep
-        
+
     def __repr__(self) -> str:
         return self.__str__()
 
@@ -46,36 +52,96 @@ class Node:
     def get_incoming(self) -> list:
         return self.incoming
 
-    # TO DO
-    def evaluate(self) -> int:
+    def _evaluate(self) -> object:
         total = 0
         for node in self.incoming:
             if node[0].uid == 0:
-                print("-> Incoming node of {}: {} (START)".format(self, node[0]))
+                print("-> Incoming node of {}: {} (START)"
+                      .format(self, node[0]))
             elif self.uid == 1:
                 print("-> Incoming node of {}: {} (END)".format(self, node[0]))
             else:
                 print("-> Incoming node of {}: {}   |".format(self, node[0]))
             total += node[0].get_output(node[1])
+
         return total + self.data
-    
-    def get_output(self, slot: int) -> int:  # change this later maybe?
+
+    def get_output(self, slot: int) -> object:
+        """
+        Getter function to return some data after a post-evaluation process.
+
+        Typically called to extract some sort of result from the current node
+        during the training process to be passed downstream towards the
+        final output node.
+
+        Args:
+            slot (int): the slot ID representing a stream of data where the
+                        output is to be passed down
+
+        Returns:
+            object: the data object to be passed down the edge identified by
+                    the user provided slot ID
+        """
         return self.evaluate()
-    
+
     # Defer to concrete class
     def check_slot(self, slot: int) -> bool:
+        """
+        A verification method to confirm if a proposed slot ID can be used.
+        
+        Typically deferred to implementing classes to manage whether a slot ID
+        can be used when a connection is to be established between two nodes.
+
+        Args:
+            slot (int): a proposed integer slot ID to be checked
+
+        Returns:
+            bool: True if the slot is not reserved and not in use, or
+                  False otherwise. Returns False by default in the Node
+                  superclass.
+        """
         return False
 
     # Default method for all concrete classes
     def add_incoming_connection(self, output_node: Node, slot: int) -> bool:
+        """
+        Adds an edge from the user-provided output node to the current node.
+
+        Args:
+            output_node (Node): the user provided node that outputs some sort
+                                of data to be used by the current node
+            slot (int): the user provided integer slot ID that is to be
+                        assigned to the edge
+
+        Returns:
+            bool: [description]
+        """
+
         if output_node.check_outgoing_connection(self, slot):
-            self.incoming.append((output_node, slot))
+            self.incoming.append(slot)
             return True
-        
+
         return False
 
     # Default method for all concrete classes
-    def check_outgoing_connection(self, input_node: Node, slot: int) -> bool:
+    def check_outgoing_connection(self, slot: int) -> bool:
+        """
+        Helper function to check connection can be added for an outgoing node.
+
+        Since the training process relies on a backtracking training algorithm,
+        the slot validation method is checked by an outgoing node which has
+        ownership over the slots assigned to outgoing edges. Is typically
+        called by the receiving node of the output, prior to appending any
+        incoming connections.
+
+        Args:
+            slot (int): the assigned integer representing the ID of the edge
+                        to be created
+
+        Returns:
+            bool: True if the slot is not reserved and not in use, or
+                  False otherwise
+        """
         if self.check_slot(slot):
             return True
         else:
