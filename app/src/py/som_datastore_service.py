69d667715a0695533201e37f7032e750796cd16b
tswu6397@uni.sydney.edu.au <tswu6397@uni.sydney.edu.au>
2021-10-27 18:17:29 +1100

    filled in datastore implementation

--- app/src/py/som_datastore_service.py
+++ app/src/py/som_datastore_service.py
@@ -1,27 +1,26 @@
-import webview
+import json
+import numpy as np
 
 class SOMDatastoreService:
     def __init__(self):
         self.cache = {}
-
-    def open_csv_file(self):
-        pass
-
-    def open_json_file(self):
-        pass
+        self.dataset_path = None
+        self.weights_path = None
+        self.dataset_points = None
+        self.weights_points = None
 
     # Calculate which epoch the percentage would result in
     # and return theose weights
     def get_som_weights_by_training_percentage(self, percentage):
-        pass
+        if not self.weights_points:
+            return None
+        n_weights = len(self.weights_points)
+        return_n = int(n_weights * percentage)
+        return self.weights_points[return_n]
 
     # Return dataset values
     def get_dataset(self):
-        pass
-
-    # Return graph output values
-    def get_graph_output(self):
-        pass
+        return self.dataset_points
 
     def get_cache_data(self, descriptor):
         if descriptor in self.cache.keys:
@@ -30,12 +29,26 @@ class SOMDatastoreService:
 
     # Return SOM dimensions
     def store_path_to_weights(self, path):
-        pass
+        self.weights_path = path
+        fields = json.loads(open(path).read())
+        self.weights_points = fields["weights"]
+        return fields["w"], fields["h"]
 
     # Return dataset dimensions
     def store_path_to_dataset(self, path):
-        pass
+        self.dataset_path = path
+        data = open(path).readlines()
+        self.dataset_points = [line.strip().split(",") for line in data]
+        np_data = np.array(data)
+        return np_data.shape
 
     def store_cache_data_with_descriptor(self, descriptor, data):
         self.cache[descriptor] = data
-        return True
+        return descriptor
+
+    # Return graph output values
+    def get_graph_output(self):
+        pass
+
+datastore = SOMDatastoreService()
+datastore.store_path_to_weights("../../../playgrounds/numba_npsom_perf_analysis/out.json")
