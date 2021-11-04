7d8c578ad424a715632baa4d1ff2ab79809a2504
Tony Huang <thua9118@uni.sydney.edu.au>
2021-11-05 10:26:43 +1100

    add test cases

--- tests/pysom_tests/test_graph.py
+++ tests/pysom_tests/test_graph.py
@@ -20,6 +20,18 @@ class TestGraph:
         assert len(g.get_nodes()) == 2
 
     def test_graph_add_single(cls):
-        props = dict()
-        nid = g.create(Node, props=props)
+        nid = g.create()
         assert nid > 1
+
+    def test_graph_add_single_default(cls):
+        nid = g.create()
+        node = g.find_node(nid)
+        assert(type(node) is Node)
+
+    def test_graph_add_multiple_default(cls):
+        assert len(g.get_nodes()) == 2
+        nid1 = g.create()
+        nid2 = g.create()
+        assert len(g.get_nodes()) == 4
+        assert (g.find_node(nid1) is not None)
+        assert (g.find_node(nid2) is not None)
