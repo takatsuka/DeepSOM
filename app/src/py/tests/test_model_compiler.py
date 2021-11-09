057440c7b5c576e3d36256f06d9850b14465c858
tswu6397@uni.sydney.edu.au <tswu6397@uni.sydney.edu.au>
2021-11-09 12:40:07 +1100

    model compiler tests not done yet

---
+++ app/src/py/tests/test_model_compiler.py
@@ -0,0 +1,21 @@
+import pytest
+
+from model_compiler import *
+
+def test_dist_props():
+    pass
+
+def test_concat_props():
+    pass
+
+def test_som_props():
+    pass
+
+def test_bmu_props():
+    pass
+
+def test_calibrate_props():
+    pass
+
+def test_parse_dict():
+    pass
