1bb0b314e6184a618a076fa30524a99ae9c4188b
HaoYan Qi <haoyanqi666@gmail.com>
2021-12-02 11:20:35 +0800

    deprecation warning

--- pysom/nodes/layer.py
+++ pysom/nodes/layer.py
@@ -8,10 +8,18 @@ from pysom.nodes.dist import Dist
 from pysom.nodes.concat import Concat
 import multiprocessing
 
+from ..deprecated import deprecated
+
+import warnings
+warnings.warn(f"""Module {__name__} have never been tested and was deprecated. 
+                  Any usages is not recommend and the behavior is undefined""",
+              DeprecationWarning, stacklevel=2)
+
 BMU_1D = "1D"
 BMU_2D = "2D"
 
 
+@deprecated
 class Layer(Node):
     """
     Node type for holding many SOMs at once.
@@ -37,7 +45,7 @@ class Layer(Node):
             will output in 1D or 2D mode. Must be either "1D" or "2D".
             Defaults to 2D
     """
-
+    @deprecated
     def __init__(self, uid: int, graph, parallel_mode: bool,
                  all_som_props: list, bmu_output: str = "2D"):
 
@@ -67,9 +75,10 @@ class Layer(Node):
             selections.append(som_input)
             layer_inlen += dim
 
-            new_bmu = self.layer_graph.create(node_type=BMU, props={"output": bmu_output})
+            new_bmu = self.layer_graph.create(
+                node_type=BMU, props={"output": bmu_output})
             all_bmus.append(new_bmu)
-            
+
             self.layer_graph.connect(new_som, new_bmu, 0)
 
         dist = self.layer_graph.create(Dist, {"selections": selections})
@@ -87,14 +96,17 @@ class Layer(Node):
         self.som_count = som_count
         self.conc = self.layer_graph.find_node(conc)
 
+    @deprecated
     def __str__(self) -> str:
         str_rep = "LayerNode {}".format(self.uid)
         return str_rep
 
+    @deprecated
     def process_route(self, i, q):
         result = self.conc.get_input(index=i)
         q.put((i, result))
 
+    @deprecated
     def _evaluate(self):
 
         self.layer_graph.set_input(self.get_input())
@@ -126,16 +138,18 @@ class Layer(Node):
                 all_bmu_outs[i] = self.conc.get_input(index=i)
 
         self.conc.precon = concatenate(all_bmu_outs, -1)
-    
+
         self.conc.output_ready = True
         self.output_ready = True
 
+    @deprecated
     def get_output(self, slot: int):
         if not self.output_ready:
             self._evaluate()
 
         return self.layer_graph.get_output()
 
+    @deprecated
     def check_slot(self, slot: int) -> bool:
         if (slot != 1):
             raise RuntimeError("Layer Node only accepts connections on slot 1")
